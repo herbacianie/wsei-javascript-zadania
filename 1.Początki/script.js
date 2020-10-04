af2873eca4282aefe119daac9cc1830902dfa1d1
herbacianie <62239265+herbacianie@users.noreply.github.com>
2020-10-04 12:44:54 +0200

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,15 @@
+//Zadanie 1
+
+console.log("Zbignie Gawroński");
+
+//Zadanie 2
+
+console.log(12+32);
+console.log(52/16);
+console.log(5*2);
+
+//Zadanie 3
+
+console.log("Hello World");
+console.log(Hello World); 
+console.log("Hello World); // tylko piewrsza linia console.log jest poprawna, przy kolejnych wywurzca błąd.
