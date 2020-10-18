f4b3fc50c3fe867dc72ccfd55447e5d9340e2eac
herbacianie <62239265+herbacianie@users.noreply.github.com>
2020-10-18 11:53:35 +0200

    Update script.js

--- 6.Funkcje/script.js
+++ 6.Funkcje/script.js
@@ -15,7 +15,29 @@ nameFunction(2console);
 // zad 3
 
 let txt = 'omg';
-function returnString= (txt) => {
+function returnString = (txt) => {
 return txt;
 }
 console.log(returnString(txt));
+
+// zad 3
+ 
+
+function myString(paramStr) {
+  
+  let counter = 0;
+  
+  let myInterval = setInterval(()=>{
+    counter ++;
+    if(counter == 5){
+      console.log('Koniec');
+      clearInterval(myInterval);
+    }
+    else{
+        console.log(paramStr);
+    }
+    
+  },3000;
+}
+
+myString('Jakiś string');
