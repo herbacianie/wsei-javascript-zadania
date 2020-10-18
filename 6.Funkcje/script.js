18405983c0731cc2df5f8dab22afe5552e819157
herbacianie <62239265+herbacianie@users.noreply.github.com>
2020-10-18 11:43:49 +0200

    Update script.js

---
+++ 6.Funkcje/script.js
@@ -0,0 +1,21 @@
+// zad 1
+let  sayHi = () => {
+  console.log("Udało się!");
+};
+
+sayHi();
+
+// zad 2
+
+function nameFunction(nr) {
+    console.log(nr)
+}
+nameFunction(2console);
+
+// zad 3
+
+let txt = 'omg';
+function returnString= (txt) -> {
+return txt;
+}
+console.log(returnString(txt));
