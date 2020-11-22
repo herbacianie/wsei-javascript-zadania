4e27eb859d9911cc55f32cc22aba59251234ca69
zgawronski <62239265+zgawronski@users.noreply.github.com>
2020-11-22 11:40:50 +0100

    Update script.js

--- 9.Konstruktory i prototype/script.js
+++ 9.Konstruktory i prototype/script.js
@@ -81,3 +81,34 @@ const game1 = new Game();
 const game2 = new Game();
 game1.changeNumber();
 game2.checkNumber();
+
+
+//lub 
+function Game(){
+    
+   this.lastNumber = 0;
+   this.number = 0;
+   this.generator = null;
+    
+    this.startGenerator = () => {
+        this.generator = setInterval(() => {
+            this.lastNumber = this.number;
+            this.number = Math.floor((Math.random() * 10) + 1);
+            
+            console.log(this.number);
+            this.checkWin();
+        }, 1000)
+    }
+    
+    this.checkWin = () => {
+        if(this.lastNumber + 5 === this.number){
+            console.log("Wygrałeś");
+            clearInterval(this.generator);
+        }
+    }
+}
+
+let game1 = new Game();
+game1.startGenerator();
+
+
