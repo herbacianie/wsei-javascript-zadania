98407aa3271ccb556194a9de0796ee422fa68157
zgawronski <62239265+zgawronski@users.noreply.github.com>
2020-11-22 08:29:09 +0100

    Update script.js

--- 4.Pętle i ify/script.js
+++ 4.Pętle i ify/script.js
@@ -127,3 +127,39 @@ console.log(result1+result2);
 
 }
 
+// d
+
+for (let x = 0; x < 5; x++) {
+    var result = '';
+    for (let y = 0; y < x + 1; y++) {
+        result += star;
+    }
+    for (let z = 0; z < 5 - (x + 1); z++) {
+        result += (z + 1);
+    }
+    console.log(result);
+}
+
+
+// e
+
+let star = ' *';
+
+for (let x = 0; x < 5; x++) {
+    var result = '';
+    for (y = 0; y < 5 - x; y++) {
+        result += ' ';
+    }
+    for (z = 0; z < x + 1; z++) {
+        result += star;
+    }
+    console.log(result);
+}
+for (let x = 0; x < 3; x++) {
+    var result = '';
+    for (y = 0; y < 5; y++) {
+        result += ' ';
+    }
+    result += star;
+    console.log(result);
+}
