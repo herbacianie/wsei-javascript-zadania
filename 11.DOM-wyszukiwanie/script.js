bc28aca3cbc3d27b560a87fbf4c9007bb06d04c6
zgawronski <62239265+zgawronski@users.noreply.github.com>
2020-11-22 13:00:39 +0100

    Update script.js

--- 11.DOM-wyszukiwanie/script.js
+++ 11.DOM-wyszukiwanie/script.js
@@ -1,8 +1,22 @@
-// zadanie 1
+// zad 1
+let links = document.getElementsByClassName('list');
 
-let links = document.getElementsByTagName('list');
+console.log(links);
 
-// zadanie 2
-let myDiv = document.getElementsByTagName('tag');
-myDiv.tagName;
+// zad 2
+
+function tagElements(tag) {
+    const elements = document.getElementsByTagName(tag);
+    for (let i = 0; i<elements.length; i++) {
+        console.log(elements[i]);
+    }
+}
+
+tagElements('ul');
+
+// zad 3
+
+const listElement = document.querySelector('#list');
+
+console.log(listElement);
 
